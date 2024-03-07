8e4f3fb29da477d8ca80e7f2e062e8416bbc5871
dd64 <87917282+dd6478@users.noreply.github.com>
2024-03-07 15:07:56 +0100

    Adding postgres and docker-compose.yml

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foto_foireuse_db',
+        'USER': 'USER',
+        'PASSWORD': 'TEST',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 # Password validation
