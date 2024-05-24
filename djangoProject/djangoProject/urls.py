1ac11fc01858bb3a69bbc50f4ce2db35c9325f5d
dd64 <87917282+dd6478@users.noreply.github.com>
2024-05-24 12:04:49 +0200

    Add reverse proxy

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -33,6 +33,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('api/', include('Account.urls')),
     path('api/', include('FotoFoireuse.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
