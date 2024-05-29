6f9b0633ad1913120ff0b69236081ec6789837c2
cypichou <cyprien.godet@gmail.com>
2024-05-29 10:50:44 +0200

    upload image v1

--- fotofront/src/components/Publication/Publication.tsx
+++ fotofront/src/components/Publication/Publication.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import FileUploadButton from "./FileUploadButton";
 
 const Publication = () => {
-  return <div>Publication</div>;
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+
+  const handleFilesSelected = (files: FileList) => {
+    setSelectedFiles(Array.from(files));
+  };
+
+  return (
+    <>
+      <FileUploadButton onFilesSelected={handleFilesSelected} />
+      <div>
+        {selectedFiles.length > 0 && (
+          <div>
+            <h2>Selected Files:</h2>
+            <ul>
+              {Array.from(selectedFiles).map((file, index) => (
+                <li key={index}>{file.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Publication;
