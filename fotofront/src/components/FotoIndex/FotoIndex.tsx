29d2a99e0128ef050227309b589e0a670c13f2a0
cypichou <cyprien.godet@gmail.com>
2024-10-04 17:47:56 +0200

    ptit logo

--- fotofront/src/components/FotoIndex/FotoIndex.tsx
+++ fotofront/src/components/FotoIndex/FotoIndex.tsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const FotoIndex = () => {
   return (
-    <div>
-      <h1 style={{ color: "white" }}>Welcome to Fotofoireuse!</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center", // Centre horizontalement
+        alignItems: "flex-start", // Aligne en haut
+        height: "100vh", // Prend toute la hauteur de la fenêtre
+        paddingTop: "20px", // Optionnel : un peu de marge en haut
+      }}
+    >
+      <img
+        src="/dragonSansFond-modified.png"
+        alt="Dragon"
+        style={{ maxWidth: "100%", height: "auto" }}
+      />
     </div>
   );
 };
