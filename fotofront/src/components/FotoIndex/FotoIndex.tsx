eb5ce61464d781c4e08f026e2264dd70f209744b
cypichou <cyprien.godet@gmail.com>
2024-10-04 10:16:06 +0200

    image reduction

--- fotofront/src/components/FotoIndex/FotoIndex.tsx
+++ fotofront/src/components/FotoIndex/FotoIndex.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const FotoIndex = () => {
   return (
     <div>
-      <h1>Welcome to Fotofoireuse!</h1>
+      <h1 style={{ color: "white" }}>Welcome to Fotofoireuse!</h1>
     </div>
   );
 };
